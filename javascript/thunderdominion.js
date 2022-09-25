eb8c46308a544b7acd39f866bad0a0d7fa29d9cf
tracerdominion <68050258+tracerdominion@users.noreply.github.com>
2022-09-24 21:14:37 -0700

    chart beginnings

--- javascript/thunderdominion.js
+++ javascript/thunderdominion.js
@@ -14,6 +14,7 @@ var notes = {
 }
 
 var wrapper = document.getElementById('wrapper');
+var charts = {};
 
 loadPage();
 
@@ -101,6 +102,7 @@ function loadPage() {
 		
 		ranks[expansion].year = ranks[expansion].year.map(y => String(y));
 		
+		//header
 		let ediv = document.createElement('div');
 		ediv.appendChild(document.createElement('hr'));
 		let ename = document.createElement('h3');
@@ -112,11 +114,25 @@ function loadPage() {
 			enote.appendChild(document.createTextNode(notes[expansion]));
 			ediv.appendChild(enote);
 		}
+		
+		//table
+		let tabdiv = document.createElement('div');
+		tabdiv.classList.add('thundertable-wrapper');
 		let tab = document.createElement('table');
 		tab.id = expansion.toLowerCase().replace(/ /g, '-') + "-table";
 		tab.classList.add('thundertable');
 		tab.appendChild(renderTable(expansion, ranks[expansion]['year'][0], false));
-		ediv.appendChild(tab);
+		tabdiv.appendChild(tab);
+		ediv.appendChild(tabdiv);
+		
+		//chart
+		let chartdiv = document.createElement('div');
+		chartdiv.classList.add('thunderchart-wrapper');
+		let chart = document.createElement('div');
+		chart.id = expansion.toLowerCase().replace(/ /g, '-').replace('+', '') + "-chart";
+		chartdiv.appendChild(chart);
+		ediv.appendChild(chartdiv);
+		renderChart(expansion);
 	}
 }
 
@@ -215,4 +231,154 @@ function sortTable(ev) {
 	let expansion = expansionTable.id.split('-').slice(0,-1).map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(' ');
 	expansionTable.innerHTML = '';
 	expansionTable.appendChild(renderTable(expansion, sortYear, desc));
+}
+
+function renderChart(expansion) {
+	var chartData = [];
+	var years = ranks[expansion]['year'].slice().reverse();
+	var maxRank = 0;
+	let expCards = Object.keys(ranks[expansion]).filter(x => x !== 'year');
+	if (expansion == 'Cornucopia + Guilds') {
+		for (card of expCards) {
+			for (let i = years.length-1; i >= 0; i--) {
+				if (ranks[expansion][card].rank[i] != -1) {
+					chartData.push({'card': card, 'year': ranks[expansion]['year'][i], 'rank': ['2018', '2019'].includes(ranks[expansion]['year'][i]) ? ranks[expansion][card].rank[i] * 2 - 0.5 : ranks[expansion][card].rank[i]});
+				}
+				if (ranks[expansion][card].rank[i] > maxRank) {
+					maxRank = ranks[expansion][card].rank[i];
+				}
+			}
+		}
+	} else {
+		for (card of expCards) {
+			for (let i = years.length-1; i >= 0; i--) {
+				if (ranks[expansion][card].rank[i] != -1) {
+					chartData.push({'card': card, 'year': ranks[expansion]['year'][i], 'rank': ranks[expansion][card].rank[i]});
+				}
+				if (ranks[expansion][card].rank[i] > maxRank) {
+					maxRank = ranks[expansion][card].rank[i];
+				}
+			}
+		}
+	}
+	
+	var spec = {
+		'height': 500,
+		'width': 700,
+		'data': {'values': chartData},
+		'layer': [
+			{
+				'mark': {
+					'type': 'line',
+					'strokeWidth': 2,
+					'stroke': '#000000'
+				},
+				'selection': {
+					'hovered': {'type': 'single', 'fields': ['card'], 'on': 'mouseover', 'empty': 'all'}
+				},
+				'encoding': {
+					'x': {
+						'field': 'year',
+						'type': 'ordinal',
+						'scale': {'domain': years},
+						'title': 'Ranking Time',
+						'axis': {'grid': true, 'labelAngle': 0}
+					},
+					'y': {
+						'field': 'rank',
+						'type': 'quantitative',
+						'scale': {
+							'reverse': true,
+							'domain': [0, maxRank + 1],
+							'nice': false
+						},
+						'title': null,
+						'axis': {'domain': false, 'labels': false, 'grid': false, 'ticks': false}
+					},
+					'strokeDash': {
+						'field': 'card',
+						'scale': {
+							'domain': expCards,
+							'range': Array.from({length: expCards.length}, () => [1,0])
+						},
+						'legend': null
+					},
+					'opacity': {'condition': {'selection': 'hovered', 'value': 1}, 'value': 0.3},
+					'tooltip': {'field': 'card'}
+				}
+			},
+			{
+				'mark': {
+					'type': 'point'
+				},
+				'encoding': {
+					'x': {
+						'field': 'year',
+						'type': 'ordinal',
+						'scale': {'domain': years}
+					},
+					'y': {'field': 'rank', 'type': 'quantitative'},
+					'color': {
+						'field': 'card',
+						'scale': {
+							'domain': expCards,
+							'range': Array.from({length: expCards.length}, () => '#000000')
+						},
+						'legend': null
+					},
+					'opacity': {'condition': {'selection': 'hovered', 'value': 1}, 'value': 0.3},
+					'tooltip': {'field': 'card'}
+				}
+			},
+			{
+				'transform': [
+					{'filter': `datum.year == '${years[years.length-1]}'`}
+				],
+				'mark': {
+					'type': 'text',
+					'align': 'left',
+					'dx': 6
+				},
+				'encoding': {
+					'x': {
+						'field': 'year',
+						'type': 'ordinal',
+						'scale': {'domain': years}
+					},
+					'y': {'field': 'rank', 'type': 'quantitative'},
+				'text': {'field': 'card'}
+				}
+			},
+			{
+				'transform': [
+					{'filter': `datum.year == '${years[0]}'`}
+				],
+				'mark': {
+					'type': 'text',
+					'align': 'right',
+					'dx': -6
+				},
+				'encoding': {
+					'x': {
+						'field': 'year',
+						'type': 'ordinal',
+						'scale': {'domain': years}
+					},
+					'y': {'field': 'rank', 'type': 'quantitative'},
+					'text': {'field': 'card'}
+				}
+			}
+		],
+		'config': {'style': {'cell': {'stroke': 'transparent'}}}
+	}
+	
+	if (expansion == 'Cornucopia + Guilds') {
+		spec.layer[3].transform.push({'filter': `indexof(${JSON.stringify(mixed.Cornucopia)}, datum.card) >= 0`});
+		spec.layer[3].mark.dy = -6;
+		spec.layer.push(JSON.parse(JSON.stringify(spec.layer[3])));
+		spec.layer[4].transform[1] = ({'filter': `indexof(${JSON.stringify(mixed.Cornucopia)}, datum.card) == -1`});
+		spec.layer[4].mark.dy = 6;
+	}
+	
+	vegaEmbed(`#${expansion.toLowerCase().replace(/ /g, '-').replace('+', '')}-chart`, spec, {"actions": false}).then(function(res) {charts[expansion] = res.view});
 }
